9c5ff0cc944c5726d8ee4f31e2326919e4fb01e5
kayacahit <cahit.kaya@elium.academy>
2017-09-15 15:58:08 +0200

    js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function bubbles() {
     var bubbles = document.getElementsByClassName("bubbles");
     for(var i = 0; i < bubbles.length; i++){
         setInterval(function() {
-            bubbles[i+1].classList.toggle("bubble");
-          }, 6000);
+            bubbles[i].classList.toggle("bubble"+[i+1]);
+          }, 2000);
           console.log(i+1);
           console.log("inside loop and setinterval");
     }
